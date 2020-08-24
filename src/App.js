6227cadd087bb0111fbcc8c474738eb876707df8
Aleksandar Djurovski <aleksandar.djurovski@it-labs.com>
2020-08-24 21:05:24 +0200

    create Header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Home from './pages/Home/Home';
 import ShopPage from './pages/Shop/Shop.tsx';
 
+import Header from './components/Header/Header.tsx'
+
 function App() {
   return (
     <div>
-      <Route exact path= '/' component={Home}/>
-      <Route exact path= '/shop' component={ShopPage}/>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
